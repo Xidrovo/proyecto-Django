ac85bdb214acdd0bcf775433340d7eca2dfda96d
Xidrovo <xidrovo@hotmail.com>
2017-02-21 12:20:55 -0500

    Creando la base de datos
    Base de datos terminado, espero todo funcione bien :(

--- programming/programacion/models.py
+++ programming/programacion/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+#use CharField when you need to limit the maximum length, TextField otherwise
+class Profesor(models.Model):
+	correo = models.CharField(max_length = 100)
+	password = models.TextField()
+	nombre = models.CharField(max_length = 50)
+	apellido = models.CharField(max_length = 50)
+	cedula = models.CharField(max_length = 10, primary_key = True)
+
+class Estudiante(models.Model):
+	ROL_CHOICE = (
+        ('R', 'Regular'),
+        ('A', 'Ayudante'),
+    )
+	correo = models.CharField(max_length = 100)
+	password = models.TextField()
+	nombre = models.CharField(max_length = 50)
+	apellido = models.CharField(max_length = 50)
+	rol = models.CharField(max_length = 1, choices = ROL_CHOICE)
+	score = models.IntegerField()
+	matricula = models.CharField(max_length = 9, primary_key = True)
+
+class Paralelo(models.Model):
+	profesor_id = models.ForeignKey(Profesor, on_delete = models.CASCADE)
+	estudiante_id = models.ForeignKey(Estudiante, on_delete = models.CASCADE)
+	paralelo = models.IntegerField()
+
+class Noticia(models.Model):
+	#titulo, descripcion, fecha, creador, tags, imagen_url
+	titulo = models.CharField(max_length = 100)
+	descrpcion = models.TextField()
+	fecha_publicacion = models.DateTimeField(auto_now_add = True)
+	tags = models.CharField(max_length = 100)
+	img_url = models.CharField(max_length = 150)
+	profesor_creador = models.ForeignKey(Profesor, null = True)
+	estudiante_creador = models.ForeignKey(Estudiante, null = True)
+
+#manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
